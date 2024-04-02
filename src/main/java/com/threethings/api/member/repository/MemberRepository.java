6c6329f0ebf4c611b0f21e290c136ec74dfdddac
devch96 <chjung1006@gmail.com>
2024-04-02 15:17:04 +0900

    refact: 로그인 기능시 roleSet과 favoriteChallengeCategories N+1 문제 해결

--- src/main/java/com/threethings/api/member/repository/MemberRepository.java
+++ src/main/java/com/threethings/api/member/repository/MemberRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.threethings.api.member.domain.Member;
@@ -14,11 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByNickname(String nickname);
 
+	@EntityGraph(attributePaths = {"roleSet", "favoriteChallengeCategories"})
 	Optional<Member> findBySocialCodeAndProvider(String socialCode, Provider provider);
-
-	@EntityGraph(attributePaths = "roleSet")
-	@Query("select m from Member m where m.id= :id")
-	Optional<Member> getMemberByIdWithRoles(@Param("id") Long id);
 
 	boolean existsByNickname(@Param("nickname") String nickname);
 }
